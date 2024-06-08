9411137a5bd9df6e87f8c80ef4968f4f80a7e484
anderssongodoy <anderssongodoygarcia@gmail.com>
2024-06-08 09:25:38 -0500

    Update serializers

--- crud/serializers.py
+++ crud/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    summary = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author']
+        fields = ['id', 'title', 'author', 'summary']
+
+    def get_summary(self, obj):
+        return f"{obj.title} by {obj.author}"

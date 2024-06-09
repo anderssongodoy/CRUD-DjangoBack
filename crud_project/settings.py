293aad30f5db2634a0b22745cc06d8a60dd45751
anderssongodoy <anderssongodoygarcia@gmail.com>
2024-06-09 10:43:52 -0500

    add swagger and redoc

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crud',
     'rest_framework',
+    'drf_yasg',
 ]
 
 from datetime import timedelta
--- crud_project/urls.py
+++ crud_project/urls.py
@@ -18,6 +18,24 @@ from django.contrib import admin
 from django.urls import path
 from crud.views import book_list, book_detail, book_create, BookListView, BookListAPIView, BookDetailView, BookDetailAPIView, PublisherListCreateView, PublisherRetrieveUpdateDestroyView, UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView, TokenObtainView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+        ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', book_list, name='book_list'),
@@ -37,8 +55,10 @@ urlpatterns = [
     path('users/<int:pk>/', UserRetrieveUpdateDestroyAPIView.as_view(), name='user-retrieve-update-destroy'),
 
     path('api/token/', TokenObtainView.as_view(), name='token_obtain'),
-]
 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
 # from rest_framework.routers import DefaultRouter
 # from crud.views import BookViewSet
 
